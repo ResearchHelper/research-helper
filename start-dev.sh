a22d77605ed912671ac626642de65ed6d258a10f
HuntFeng <huntfeng@pop-os.localdomain>
2022-08-28 23:13:03 -0600

    basic note editor

--- start-dev.sh
+++ start-dev.sh
@@ -1,6 +1,7 @@
 # start backend server
 ./backend/linux-env/bin/python ./backend/app.py &
 # start vue devtools with no sandbox mode
-yarn run vue-devtools --no-sandbox &
+# yarn run vue-devtools --no-sandbox &
 # start quasar dev 
-quasar dev -m electron --devtools
+# quasar dev -m electron --devtools
+quasar dev -m electron
