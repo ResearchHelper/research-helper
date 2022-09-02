8a385d8410533dbf87e4aec550b0e7e006f15751
HuntFeng <huntfeng@pop-os.localdomain>
2022-09-02 10:13:03 -0600

    index.js for backend is added

---
+++ src/backend/index.js
@@ -0,0 +1,40 @@
+// TODO: Load the userDataPath from pinia first
+
+// local storage
+function loadTree() {
+  // TODO
+}
+
+function saveTree() {
+  // TODO
+}
+
+function addProject() {
+  // TODO
+}
+
+function deleteProject(deleteFromDB) {
+  // TODO
+}
+
+function loadPDFState() {
+  // TODO
+}
+
+function savePDFStates() {
+  // TODO
+}
+
+// api
+function extractContent() {
+  // TODO
+}
+
+export {
+  loadTree,
+  saveTree,
+  addProject,
+  deleteProject,
+  loadPDFState,
+  savePDFStates,
+};
