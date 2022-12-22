b7a9f060313be59e741cc68413fd8fda7a8e2d0c
HuntFeng <huntfeng@pop-os.localdomain>
2022-12-22 00:20:44 -0600

    remove annotStore

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,10 +4,6 @@ const routes = [
     component: () => import("layouts/MainLayout.vue"),
     children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
   },
-  // { path: "/", component: () => import("pages/LibraryPage.vue") },
-  // { path: "/", component: () => import("pages/ReaderPage.vue") },
-  // { path: "/", component: () => import("pages/NotePage.vue") },
-  // { path: "/", component: () => import("pages/SettingPage.vue") },
   // Always leave this as last one,
   // but you can also remove it
   // {
